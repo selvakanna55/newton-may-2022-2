e3fdf53e9eeff9c47329cdf710a34b1bb2d967fc
selvakanna55 <selvakanna2409@gmail.com>
2022-05-23 23:35:12 +0530

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,5 @@
 class HelloWorld{
-
-
-
-    //thrth
     public static void main(String[] args) {
-        String word = "Hello World";
-        System.out.println(word);
+        System.out.println("Hello World");
     }
 }
