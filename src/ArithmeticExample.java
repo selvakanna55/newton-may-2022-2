419ac248c304563baf823d6a4d4faa22c421eff3
selvakanna55 <selvakanna2409@gmail.com>
2022-05-23 23:32:04 +0530

    add Day1 - Programming  basics

---
+++ src/ArithmeticExample.java
@@ -0,0 +1,15 @@
+ class ArithmeticExample {
+     public static void main(String[] args) {
+          int number1 = 10;
+          int number2 = 2;
+
+          int sumOfTwoNumbers = number1+number2;
+          int diffOfTwoNumbers = number1-number2;
+          int mulOfTwoNumbers = number1*number2;
+          int divOfTwoNumbers = number1/number2;
+          System.out.println(sumOfTwoNumbers);
+          System.out.println(diffOfTwoNumbers);
+          System.out.println(mulOfTwoNumbers);
+          System.out.println(divOfTwoNumbers);
+     }
+}
---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+class HelloWorld{
+
+
+
+    //thrth
+    public static void main(String[] args) {
+        String word = "Hello World";
+        System.out.println(word);
+    }
+}
